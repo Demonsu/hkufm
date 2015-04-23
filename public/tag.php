eba73f44768d7df6cdaefedf15498aa85e1edeb5
Demonsu <245681117@qq.com>
2015-04-23 19:10:32 +0800

    add shortcut

--- public/tag.php
+++ public/tag.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
+	<link rel="icon" href="./img/hkufm.ico" type="image/x-icon" /> 
+	<link rel="shortcut icon" href="./img/hkufm.ico" type="image/x-icon" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>Tag Search</title>
 </head>
 <body>
 
